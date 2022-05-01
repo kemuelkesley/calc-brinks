6196749d86eea5164979cd4a58a1d1f5decfe625
kemuelkesley <kemuelkesleytkd@outlook.com>
2022-05-01 13:41:37 -0300

    Foi incluido novas opções, para selecionar (somar, subtrair, multiplicar e dividir)

--- script.js
+++ script.js
@@ -1,22 +1,90 @@
+// passos para na hora que escolher a opcao fazer o calculo
+function opc(){
+
+    let select = document.querySelector('#escolherOpcao');
+
+    let value = select.options[select.selectedIndex].value;
+    console.log(value);
+
+
+    // somar
+    if (value == 's') {
+        console.log('Estou somando.')
+        somar();
+        console.log(somar());
+    }
+    
+    // Subtrair
+    if (value == 'sub') {
+        console.log('Estou subtraindo.')
+        subtrair();
+        console.log(subtrair());
+    }
+    
+
+    // Multiplicar
+    if (value == 'm') {
+        console.log('Estou multiplicando.')
+        multiplicar();
+        console.log(multiplicar());
+    }
+
+
+    // Dividir
+    if (value == 'd') {
+        console.log('Estou multiplicando.')
+        dividir();
+        console.log(dividir());
+    }
+
+}
+
+// somar
+
 function somar(){
+   
     var numberA = document.querySelector('#numero-a').value;  
     var numberB = document.querySelector('#numero-b').value;
     var total = parseFloat(numberA) + parseFloat(numberB);
     var mostrarResultado = document.getElementById('resultado').value = total;
     mostrarResultado;     
-    console.log(mostrarResultado);    
-
+    console.log('total=',numberA,'+',numberB,'=', mostrarResultado);  
 } 
 
 
-function multiplicar(){
-    var numberA = document.getElementById('numero-a').value;
-    var numberB = document.getElementById('number-b').value;
-    var total = parseInt(numberA * numberB).value = total;
-    console.log(total);
+// subtrair
+
+function subtrair(){
+    var numberA = document.querySelector('#numero-a').value;
+    var numberB = document.querySelector('#numero-b').value;
+    var total = parseFloat(numberA) - parseFloat(numberB);
+    var mostrarResultado = document.querySelector('#resultado').value = total;
+    mostrarResultado;
+    console.log('total=',numberA,'-',numberB,'=', mostrarResultado);  
 }
 
-// Quando clicar no botão calcular e div vai aparecer.
+// multiplicar
+
+function multiplicar(){
+    var numberA = document.querySelector('#numero-a').value;
+    var numberB = document.querySelector('#numero-b').value;
+    var total = parseFloat(numberA) * parseFloat(numberB);
+    var mostrarResultado = document.querySelector('#resultado').value = total;
+    mostrarResultado;
+    console.log('total=',numberA,'*',numberB,'=', mostrarResultado); 
+}
+
+function dividir(){
+    var numberA = document.querySelector('#numero-a').value;
+    var numberB = document.querySelector('#numero-b').value;
+    var total = parseFloat(numberA) / parseFloat(numberB);
+    var mostrarResultado = document.querySelector('#resultado').value = total;
+    mostrarResultado;
+    console.log('total=',numberA,'/',numberB,'=', mostrarResultado);  
+}
+
+
+// Quando clicar no botão calcular a div vai aparecer.
 
 var btnCalcular = document.querySelector('.button__calc');
 var container = document.querySelector('.show-or-hide');
@@ -31,7 +99,7 @@ btnCalcular.addEventListener('click', function(){
 
 });
 
-// Quando limpar clicar no botao limprar o campo some
+// Quando clicar no botao limpar o campo some
 
 var btnLimpar = document.querySelector('.large-button');
 var containerCalc = document.querySelector('.show-or-hide');
